5ba33fad8e62a6fa2a6c8e6d1e758b6b223c2c5d
jianghua.yjh <jianghua.yjh@alibaba-inc.com>
2017-02-17 14:28:51 +0800

    TODO branch

--- controllers/mapreduce.js
+++ controllers/mapreduce.js
@@ -7,14 +7,13 @@ module.exports.getAvgPm25ByID = function(query, callback) {
     var o = {}, self = this;
     o.query = query;
     o.map = function () {
-        // var ts = this.updated;
-        // var time = new Date(ts.getUTCFullYear(), ts.getUTCMonth(), ts.getUTCDate());
-        // time = time.toLocaleDateString();
-        // emit({ts: time, sensorId: this.sensorId }, this.sum/this.count);
-
+        var ts = this.updated;
+        var time = new Date(ts.getUTCFullYear(), ts.getUTCMonth(), ts.getUTCDate());
+        time = time.toLocaleDateString();
+        emit({ts: time, sensorId: this.sensorId }, this.sum/this.count);
     };
     o.reduce = function (key, values) {
-        
+        return Array.sum(values) / values.length;
     };
 
     o.out = { replace: 'avgPm25ByID' };
