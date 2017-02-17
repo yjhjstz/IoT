acbdf6007136deb9aba4e8b49973f9aa2f656c96
jianghua.yjh <jianghua.yjh@alibaba-inc.com>
2017-02-17 14:28:51 +0800

    TODO branch

--- controllers/mapreduce.js
+++ controllers/mapreduce.js
@@ -2,18 +2,19 @@
 var Air = require('../models/air');
 
 
-
+// TODO map / reduce
 module.exports.getAvgPm25ByID = function(query, callback) {
     var o = {}, self = this;
     o.query = query;
     o.map = function () {
-        var ts = this.updated;
-        var time = new Date(ts.getUTCFullYear(), ts.getUTCMonth(), ts.getUTCDate());
-        time = time.toLocaleDateString();
-        emit({ts: time, sensorId: this.sensorId }, this.sum/this.count);
+        // var ts = this.updated;
+        // var time = new Date(ts.getUTCFullYear(), ts.getUTCMonth(), ts.getUTCDate());
+        // time = time.toLocaleDateString();
+        // emit({ts: time, sensorId: this.sensorId }, this.sum/this.count);
+
     };
     o.reduce = function (key, values) {
-        return Array.sum(values) / values.length;
+        
     };
 
     o.out = { replace: 'avgPm25ByID' };
--- models/air.js
+++ models/air.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// TODO schema
 const airSchema = new Schema({
-    sensorId: { type: Number, default: 0 },
-    updated: { type: Date, default: Date.now },
-    count: { type: Number, default: 0 },
-    sum: { type: Number, default: 0 },
-    pm25: [ {} ],
-    probe: String,
-    location: []
+    
 });
-
-airSchema.index({updated:-1, sensorId:1}, {unique: true}); // schema level
 
 const ModelClass = mongoose.model('air', airSchema);
 
